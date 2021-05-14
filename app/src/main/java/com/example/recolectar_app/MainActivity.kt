4cfbf0f8df1bcf6dfb528d25cbcb0bb0755932a6
Cristian Pedrozo <cristian.pedrozo@liberis.co.uk>
2021-05-13 21:20:49 -0300

    Se creo la activity

--- app/src/main/java/com/example/recolectar_app/MainActivity.kt
+++ app/src/main/java/com/example/recolectar_app/MainActivity.kt
@@ -6,15 +6,28 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+    var numero = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun login(view: View) {
-        val email = findViewById<EditText>(R.id.login_email).text.toString()
+        if(numero == 1) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            delegate.applyDayNight()
+            numero = 0;
+        }
+        else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            delegate.applyDayNight()
+            numero = 1;
+        }
+
+        /*val email = findViewById<EditText>(R.id.login_email).text.toString()
         val password = findViewById<EditText>(R.id.login_password).text.toString()
         //hacer llamado a fiware para corroborar si el usuario existe, by email.
 
@@ -28,6 +41,6 @@ class MainActivity : AppCompatActivity() {
             //startActivity(intent)
         } else {
             Toast.makeText(this, "Email o contraseña invalidos", Toast.LENGTH_SHORT).show()
-        }
+        }*/
     }
 }
