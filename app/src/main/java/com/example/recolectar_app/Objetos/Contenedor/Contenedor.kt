a1e2a0244307035c68c0ac9bf3edb0cb00ec715a
vanessalevano2020 <lesvanell@gmail.com>
2021-05-21 20:27:21 -0300

    Listado Contenedores y Camiones

--- app/src/main/java/com/example/recolectar_app/Objetos/Contenedor/Contenedor.kt
+++ app/src/main/java/com/example/recolectar_app/Objetos/Contenedor/Contenedor.kt
@@ -1,10 +1,14 @@
 package com.example.recolectar_app.Objetos.Contenedor
 
 data class Contenedor(
+        /*
         val category: Category,
         val dateLastEmptying: DateLastEmptying,
         val fillingLevel: FillingLevel,
+        */
+
         val id: String,
+        /*
         val location: Location,
         val nextActuationDeadline: NextActuationDeadline,
         val refDevice: RefDevice,
@@ -14,4 +18,6 @@ data class Contenedor(
         val status: Status,
         val temperature: Temperature,
         val type: String
+                */
+
 )
--- app/src/main/java/com/example/recolectar_app/administrador/Camiones.kt
+++ app/src/main/java/com/example/recolectar_app/administrador/Camiones.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.recolectar_app.R
+import com.example.recolectar_app.adapters.CamionListAdapter
+import com.example.recolectar_app.entities.Camion
+import com.google.android.material.snackbar.Snackbar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +23,65 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Camiones : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    lateinit var v: View
+
+    lateinit var recCamiones: RecyclerView
+
+    var camiones: MutableList<Camion> = ArrayList<Camion>()
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
+    private lateinit var camionListAdapter: CamionListAdapter
+
+    companion object {
+        fun newInstance() = Camiones()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_administrador_camiones, container, false)
+        v = inflater.inflate(R.layout.list_fragment, container, false)
+        recCamiones = v.findViewById(R.id.rec_camiones)
+        return v
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_administrador_camiones.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Camiones().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        for (i in 1..5) {
+            camiones.add(Camion("BNG0989", "Ecologico"))
+            camiones.add(Camion("BNG0989", "Ecologico"))
+            camiones.add(Camion("BNG0989", "Ecologico"))
+            camiones.add(Camion("BNG0989", "Ecologico"))
+
+        }
+
+        //Configuración Obligatoria
+        recCamiones.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(context)
+        recCamiones.layoutManager = linearLayoutManager
+
+
+        camionListAdapter = CamionListAdapter(camiones);
+
+        /*
+        contactoListAdapter = ContactoListAdapter(contactos) { x ->
+            onItemClick(x)
+        }*/
+
+        recCamiones.adapter = camionListAdapter
+
+    }
+
+    fun onItemClick(position: Int): Boolean {
+        Snackbar.make(v, position.toString(), Snackbar.LENGTH_SHORT).show()
+        return true
     }
 }
+
