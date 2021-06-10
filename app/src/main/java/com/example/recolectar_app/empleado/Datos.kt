509cf91486ab8ddd177ab863bec399649a215d01
Cristian Pedrozo <cristian.pedrozo@liberis.co.uk>
2021-06-10 07:44:51 -0300

    Se agregaron placeholders en usuarios

--- app/src/main/java/com/example/recolectar_app/empleado/Datos.kt
+++ app/src/main/java/com/example/recolectar_app/empleado/Datos.kt
@@ -5,48 +5,73 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.TextView
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.recolectar_app.R
+import com.example.recolectar_app.Usuario
+import com.example.recolectar_app.administrador.Datos_AdministradorArgs
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class Datos : Fragment() {
     private lateinit var v: View
-    var url = "http://46.17.108.122:1026/v2/entities/empleado:1?options=keyValues"
-//    var url = "http://46.17.108.122:1026/v2/entities/?type=Empleado"
     private lateinit var datos : TextView
 
-
-
-
+    val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
         v = inflater.inflate(R.layout.fragment_empleado_datos, container, false)
-        datos = v.findViewById(R.id.txtdatos)
-        val queue = Volley.newRequestQueue(activity)
 
-        val jsonObjectRequest = JsonObjectRequest(url, null,
-            { response ->
-                datos.text = "La respuesta es: ${response}" },
-            { response ->
-                datos.text = "La respuesta es: ${response}"}
-        )
-        queue.add(jsonObjectRequest)
+        var email = getUserInstance()
+        if(email != null)
+            obtenerUsuarioFirebase(email.toString())
+
         return v
 
     }
+    fun obtenerUsuarioFirebase(email:String?){
+        if(email != null){
+            email.toString()
+            db.collection("usuarios").document(email)
+                .get()
+                .addOnSuccessListener {document->
+                    var usuario = Usuario(document.getString("razonSocial"), email, document.getString("distrito"), document.getString("jefe"), document.getString("horarioEntrada"),document.getString("horarioSalida"), document.getBoolean("esAdmin"),"https://www.uclg-planning.org/sites/default/files/styles/featured_home_left/public/no-user-image-square.jpg?itok=PANMBJF-")
+                    cargarCampos(usuario)
+                }
+        }
+    }
 
+    fun cargarCampos(usuario : Usuario){
+        v.findViewById<EditText>(R.id.editTextRazonSocial).setText(usuario.razonSocial)
+        v.findViewById<EditText>(R.id.editTextEmail).setText(usuario.email)
+        v.findViewById<EditText>(R.id.editTextJefe).setText(usuario.jefe)
+        v.findViewById<EditText>(R.id.editTextDistrito).setText(usuario.distrito)
+        v.findViewById<EditText>(R.id.editTextHorarioEntrada).setText(usuario.horarioEntrada)
+        v.findViewById<EditText>(R.id.editTextHorarioSalida).setText(usuario.horarioSalida)
+    }
+
+    fun getUserInstance(): String?{
+        var email:String? = null
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null)
+            email = user.email
+
+        return email
+    }
 
     companion object {
 
