76e57d73f7d5a348514b052e30d4d728d0347b95
Rodrigo <rdrg.000@gmail.com>
2021-06-27 04:04:10 -0300

    Splash Screen

---
+++ app/src/main/java/com/example/recolectar_app/ui/view/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.example.recolectar_app.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
